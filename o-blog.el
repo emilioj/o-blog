ac975afd7532029f73c49a65b774efda3c8291b0
Emilio J. Padron <emilioj@udc.es>
2017-04-21 12:10:30 +0200

    Add org-html-export-as-html call for compatibility with new org-mode

--- o-blog.el
+++ o-blog.el
@@ -725,7 +725,8 @@ headers and body."
 		  (ignore-errors (org-export-as-html nil nil nil 'string t))
 		  (ignore-errors (org-export-as-html nil nil 'string t))
 		  (ignore-errors (org-export-as 'html nil nil t nil))
-		  (org-export-as-html nil nil 'string nil nil))))
+		  (ignore-errors org-export-as-html nil nil 'string nil nil)
+		  (org-html-export-as-html nil nil 'string nil nil))))
       (when saved-file
 	(delete-file saved-file))
       ret)))
